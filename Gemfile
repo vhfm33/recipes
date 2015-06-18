778985e90ac5b215557d23b26baec74b4920b811
vitor hugo <vhfm33@gmail.com>
2015-06-18 11:52:08 -0300

    Setting pg

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '4.2.0'
+gem 'pg'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
@@ -16,13 +17,9 @@ gem 'devise', '~> 3.5.1'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
-  gem 'debugger'
   gem 'web-console', '~> 2.0'
   gem 'spring'
-  gem 'sqlite3'
-end
-
-
-group :production do
-  gem 'pg'
+  gem 'rspec-rails'
+  gem 'pry'
+  gem 'pry-nav'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150616184302) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "directions", force: :cascade do |t|
     t.text     "step"
     t.integer  "recipe_id"
@@ -20,7 +23,7 @@ ActiveRecord::Schema.define(version: 20150616184302) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "directions", ["recipe_id"], name: "index_directions_on_recipe_id"
+  add_index "directions", ["recipe_id"], name: "index_directions_on_recipe_id", using: :btree
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
@@ -29,7 +32,7 @@ ActiveRecord::Schema.define(version: 20150616184302) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "ingredients", ["recipe_id"], name: "index_ingredients_on_recipe_id"
+  add_index "ingredients", ["recipe_id"], name: "index_ingredients_on_recipe_id", using: :btree
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
@@ -58,7 +61,9 @@ ActiveRecord::Schema.define(version: 20150616184302) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "directions", "recipes"
+  add_foreign_key "ingredients", "recipes"
 end
